e0f7289b716a5e13586795eeddf3a0be91b58865
Lubomír Blažek <lubomir.blazek@newlogic.cz>
2023-06-22 16:45:36 +0200

    fix: types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,5 +1,5 @@
 export interface PluginUserConfig {
-    import?: import('@vituum/vite-plugin-tailwind/types/postcssImport.d.ts').AtImportOptions
+    import?: import('@vituum/vite-plugin-tailwindcss/types/postcssImport.d.ts').AtImportOptions
     nesting?: import('postcss-nesting').pluginOptions
     customMedia?: import('postcss-custom-media').pluginOptions
     autoprefixer?: import('autoprefixer').Options
